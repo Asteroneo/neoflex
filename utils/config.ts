35ca2aebd698d364f84cc11d8ec6a5f6686a23f7
AsteroNEO <asteroneox@gmail.com>
2024-10-05 15:46:44 +0200

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,14 +1,12 @@
-import { mainnet, bsc } from "wagmi/chains";
 import { neox, neoxTestnet } from "./chains/neoxChain";
 
-export const ETH_CHAINS = [mainnet, bsc, neox, neoxTestnet];
+export const ETH_CHAINS = [neoxTestnet, neox];
 export const WALLET_CONNECT_PROJECT_ID =
   process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID || "";
 
-export const SITE_NAME = "boilr3";
-export const SITE_DESCRIPTION =
-  "The Ultimate Next.js dApp Boilerplate with RainbowKit, Tailwind CSS & WAGMI";
-export const SITE_URL = "https://boilr3.vercel.app";
+export const SITE_NAME = "NeoFlex";
+export const SITE_DESCRIPTION = "The best Liquid Staking protocol on NEOX";
+export const SITE_URL = "https://neoflex.app";
 
 export const SOCIAL_TWITTER = "Envoy_1084";
 export const SOCIAL_GITHUB = "Envoy-VC/boilr3";
